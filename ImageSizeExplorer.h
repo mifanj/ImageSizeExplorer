4ce6b1d4d59979a27a38b4c6bbaafdbba022c736
mifanJ <jy2211@qq.com>
2016-01-22 16:12:38 +0800

    no message

---
+++ ImageSizeExplorer.h
@@ -0,0 +1,15 @@
+//
+//  ImageSizeExplorer.h
+//  WKMoments
+//
+//  Created by Jack on 16/1/21.
+//  Copyright © 2016年 WenKang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageSizeExplorer : NSObject
+
++ (CGSize)getImageSizeWithURL:(id)imageURL;
+
+@end
